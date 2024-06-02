c4dd9a7a7561a27e2f74c443eb14131ce5760e99
Gerard Louis Recinto <gerardrecinto@gmail.com>
2024-06-01 19:45:22 -0700

    32:42

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -8,7 +9,7 @@ const Hero = () => {
         <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill="white" />
         <Spotlight className='-top-10 -left-full h-[80vh] w-[50vw]' fill="purple" />
         <Spotlight className='-top-28 left-80 h-[80vh] w-[50vw]' fill="blue" />
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0 ">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0 ">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         
         </div>
@@ -18,7 +19,11 @@ const Hero = () => {
             <h2 className='absolute uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
               Dynamic Web Magic with Next.js
             </h2>
-          
+            <TextGenerateEffect
+              className='text-center text-[40px] md:text-5xl lg:text-6xl'
+              words='Transforming Concepts into Seamless Experiences'
+            />
+            <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>Hi, I&apos;m Gerard, a Senior Engineer based in San Diego.</p>
 
         </div>
 
