caaefafc6c5aea2b1d3d9ddcf7cc8b0609454a03
Gerard Louis Recinto <gerardrecinto@gmail.com>
2024-06-06 01:11:20 -0700

    34:44

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -1,4 +1,28 @@
-import React from 'react'
+import React from 'react';
+
+interface MagicButtonProps {
+  title: string;
+  //icon: React.ReactNode;
+  position: string;
+  handleClick?: () => void;
+  otherClasses?: string;
+}
+
+const MagicButton: React.FC<MagicButtonProps> = ({ title, icon, position, handleClick, otherClasses }) => {
+  return (
+    <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none md:w-60 md:mt-10">
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}">
+            {title}
+        </span>
+</button>
+  );
+};
+
+export default MagicButton;
+
+/*
+import React from 'react';
 
 const MagicButton = (title, icon, position, handleClick, otherClasses) => {
     title: string; icon: React.ReactNode, position: string; handleClick?: () => void; otherClasses?: string;
@@ -6,10 +30,11 @@ const MagicButton = (title, icon, position, handleClick, otherClasses) => {
     <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none md:w-60 md:mt-10">
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl ${otherClasses}">
-            Border Magic
+            {title}
         </span>
 </button>
   )
 }
 
 export default MagicButton
+*/
